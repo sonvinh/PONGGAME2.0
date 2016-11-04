0f57e0c47cf61b05ebf8c73bf9a62c7a875e7a31
vtkimchi <vtkchi1297@gmail.com>
2016-11-04 16:12:39 +0700

    fix #9 set Blue for score of two players on line 234

--- src/PongPanel.java
+++ src/PongPanel.java
@@ -233,6 +233,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 			g.drawLine(playerTwoLeft, 0, playerTwoLeft, getHeight());
 
 			// draw the scores
+			g.setColor(Color.BLUE);
 			g.setFont(new Font(Font.DIALOG, Font.BOLD, 36));
 			g.drawString(String.valueOf(playerOneScore), 100, 100); // Player 1
 																	// score
