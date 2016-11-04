5b19935afe6ce973f872991138da94ec53797a81
sonvinh <sonvinh06@gmail.com>
2016-11-04 15:44:22 +0700

    fix #5 change sPressed for sPressed on line 308

--- src/PongPanel.java
+++ src/PongPanel.java
@@ -305,7 +305,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 		} else if (e.getKeyCode() == KeyEvent.VK_W) {
 			wPressed = false;
 		} else if (e.getKeyCode() == KeyEvent.VK_S) {
-			wPressed = false;
+			sPressed = false;
 		}
 	}
 
